97ef6fa5054b539fb8a704ea42b64354c3e071d6
lw-cdm <linhtm@cdmteck.com>
2022-12-07 14:58:23 +0700

    Init Storybook

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-import './rn-kitchen-sink/App'
+/** @format */
+
+import { AppRegistry } from 'react-native'
+import App from './rn-kitchen-sink/App'
+// import App from './storybook';
+import { name as appName } from './app.json'
+
+AppRegistry.registerComponent(appName, () => App)
